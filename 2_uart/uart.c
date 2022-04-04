d8665c354a1b8f3bfe7582ce0ac1375cfb7e7034
ronja kræmer <rkhansen01@gmail.com>
2022-04-04 11:57:31 +0200

    write code and fix folders
    Signed-off-by: ronja kræmer <rkhansen01@gmail.com>

---
+++ 2_uart/uart.c
@@ -0,0 +1,28 @@
+#include "uart.h"
+#include "gpio.h"
+
+void uart_init(void){
+    GPIO0->PIN_CNF[6] = 0;  // RX, input
+    GPIO1->PIN_CNF[8] = 1;  // TX, output
+    UART->PSEL_TXD = 8 | 1 << 5;
+    UART->PSEL_RXD = 6;
+    UART->BAUDRATE = 0x00275000;
+    UART->PSEL_RTS = 1 << 31;
+    UART->PSEL_CTS = 1 << 31;
+    UART->ENABLE = 4;
+    UART->TASKS_STARTRX = 1;
+
+    return;
+}
+
+void uart_send(char letter) {
+    UART->TASKS_STARTTX = 1;
+    UART->TXD = letter;
+    while(!UART->EVENTS_TXDRDY) {
+        continue;     
+    }
+    UART->EVENTS_TXDRDY = 0;
+    UART->TASKS_STOPTX = 1;
+
+    return;
+}
